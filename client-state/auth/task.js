e6fd88a6f27b3653d1c255cb86c36103348ac45c
Roman Igoshev <Roman.parkflyer@mail.ru>
2023-06-05 21:36:02 +0500

    Задание выполнено

---
+++ client-state/auth/task.js
@@ -0,0 +1,42 @@
+const signin = document.getElementById('signin');
+const form = document.getElementById('signin__form');
+const welcome = document.getElementById('welcome');
+const userId = document.getElementById('user_id');
+const localUserId = localStorage.getItem('userId');
+
+function authorization(id) {
+    signin.classList.toggle('signin_active');
+    welcome.classList.toggle('welcome_active');
+    userId.innerText = id;
+}
+
+if (localUserId != null) {
+    authorization(localUserId);
+}
+
+form.addEventListener('submit', function (event) {
+    let formData = new FormData(form);
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth', true);
+    xhr.send(formData);
+
+    xhr.addEventListener('load', () => {
+        let auth = xhr.response;
+
+        if (auth.success) {
+            localStorage.setItem('userId', auth.user_id);
+            authorization(auth.user_id);
+        } else {
+            alert('Неверный логин/пароль');
+            form.reset();
+        }
+    });
+    event.preventDefault();
+});
+
+signOut.addEventListener('click', function () {
+    localStorage.removeItem('userId');
+    authorization(null);
+    form.reset();
+});
